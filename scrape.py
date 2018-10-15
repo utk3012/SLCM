4008f1068106e08b178380f497dd807d23e568d5
Utkarsh Raj <utkarsh3012@gmail.com>
2018-10-15 19:42:03 +0530

    marks retrieve endpoint implemented

--- scrape.py
+++ scrape.py
@@ -58,5 +58,47 @@ def attendance():
 	else:
 		return (jsonify({"name": name, "reg_no": reg_no, "roll_no": roll_no, "sem": sem, "no_of_subs": len(att), "attendance": att}), 200)
 
+@app.route('/marks', methods=['POST'])
+def marks():
+	if not request.json or not 'username' in request.json or not 'password' in request.json:
+		return (jsonify({"msg": "invalid request or missing parameters in request", "success": 0}), 400)
+	try:
+		soup = getPage(request.json['username'], request.json['password'])
+		table = soup.find("table" ,id="tblAttendancePercentage")
+		if not table:
+			return (jsonify({"msg": "Attendance has not been uploaded.", "success": 0}), 400)
+		rows = table.findChildren(['th', 'tr'])
+		subj = []
+		vals = []
+		for row in rows:
+			cells = row.findChildren('td')
+			if len(cells) > 0:
+				vals = []
+				for ind, cell in enumerate(cells):
+					if ind in [1, 2]:
+						value = cell.string
+						vals.append(value)
+					elif ind > 2:
+						continue
+				subj.append(vals)
+		marks = []
+		for sub in subj:
+			vals = {}
+			vals['Subject'] = sub[0]
+			subDiv = soup.find("div", id=sub[1].replace(' ',''))
+			subTd = subDiv.find_all("td")
+			if not subTd:
+				vals['avl'] = False
+			else:
+				vals['avl'] = True
+			vals['Marks'] = []
+			for ind, cell in enumerate(subTd):
+				vals['Marks'].append(cell.string)
+			marks.append(vals)
+	except:
+		return (jsonify({"msg": "Cannot access SLCM at the moment. Try again later!", "success": 0}), 200)
+	else:
+		return (jsonify({"marks": marks}), 200)
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000)
